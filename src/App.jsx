4d4f7463f10973c946809d24ef3ee176fd570454
Christoffer Jansson <christofferjansson1@gmail.com>
2025-01-02 19:33:56 +0100

    button and reusable code/styling setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import ButtonGradient from '/src/assets/assets/svg/ButtonGradient';
+import Button from './components/Button';
 
-import './App.css'
-
-function App() {
+const App = () => {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
-  )
-}
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello World!
+      </h1>
+
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+
+      <ButtonGradient></ButtonGradient>
+    </>
+  );
+};
 
 export default App
