3ec75b83fbed28e730ceb2e8d7f7bea19c7aee02
Christoffer Jansson <christofferjansson1@gmail.com>
2025-01-10 20:54:09 +0100

    benefits

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from './components/Button';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Benefits from './components/Benefits';
+
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
 const App = () => {
 
@@ -10,8 +11,9 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
-      <ButtonGradient></ButtonGradient>
+      <ButtonGradient />
     </>
   );
 };
