b21ae1d9243a32b38dcdacd0da30f1dfb36ca46e
Christoffer Jansson <christofferjansson1@gmail.com>
2025-01-25 11:57:34 +0100

    roadmap ui done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Benefits from './components/Benefits';
 import Collaboration from './components/Collaboration';
 import Services from './components/Services';
 import Pricing from './components/Pricing';
+import Roadmap from './components/Roadmap';
 
 import ButtonGradient from "./assets/svg/ButtonGradient";
 
@@ -18,6 +19,7 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </>
